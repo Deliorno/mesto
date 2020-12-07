97c54163a7c6df2415d50b3ca31af34c0ed3f235
Marina Lifanova <deliorno@gmail.com>
2020-12-07 16:12:16 +0300

    fixed everything

--- script/index.js
+++ script/index.js
@@ -1,5 +1,3 @@
-
-let settings = document.querySelector('#settings');
 const popup =  document.getElementById('popup');
 
 const closeCross= document.querySelectorAll('.popup__close-cross');
@@ -52,81 +50,36 @@ const initialCards = [
 ];
 
 function GalleryItems() {
-initialCards.forEach(item => {
-    let galleryItem = galleryTemplate.cloneNode(true);
-    console.log(item.name);
-    console.log(item.link);
-
-    galleryItem.querySelector('.gallery__item-pic').src = item.link;
-    galleryItem.querySelector('.gallery__item-title').textContent = item.name;
-
-    galleryItem.querySelector('.gallery__item-like').addEventListener('click', function() {
-        let target = event.target;
-        if (target.classList == "gallery__item-like"){
-            target.classList.add("gallery__item-like_active");
-        }else{
-            target.classList.remove("gallery__item-like_active");
-        } 
+initialCards.reverse().forEach(item => {
+    addCard(item.link,item.name)
     });
-    
-    galleryItem.querySelector('.gallery__item-pic').addEventListener('click', function() {
-        let target = event.target;
-        if(target.classList == 'gallery__item-pic'){
-            console.log("Nryek yf rfhnbyre");
-            let subtitle = target.closest('.gallery__item').querySelector('.gallery__item-title').textContent;
-            let image = target.closest('.gallery__item').querySelector('.gallery__item-pic').src;
-            popupFullImage.src = image;
-            popupSubtitle.textContent = subtitle;
-            popupImage.classList.add('popup_display_flex');
-        }
-    });
-
-    galleryItem.querySelector('.gallery__trash-bin').addEventListener('click', function() {
-        let target = event.target;
-        if(target.classList == 'gallery__trash-bin'){
-            target.closest('.gallery__item').remove();
-            initialCards.forEach(function(item,i) {
-                if (item.name === target.closest('.gallery__item').querySelector('.gallery__item-title').textContent){
-                    initialCards.splice(i,1);
-                }
-            })
-        }
-    });
-    console.log(document.querySelector('.gallery'));
-    document.querySelector('.gallery').append(galleryItem); 
-});
 };
+
 GalleryItems();
 
-function closeOpenPopup(){
-    let target = event.target;
-    if (target.classList == 'profile__add-btn'){
-        popupCard.classList.add('popup_display_flex');
-    }
-    if (target.classList == 'profile__settings'){
-        popup.classList.add('popup_display_flex');
-    }
-    if(target.closest('.popup')){
-        target.closest('.popup').classList.remove('popup_display_flex');
-    }
-
+function addCardFormSubmit(evt){
+    evt.preventDefault(); 
+    addCard(linkInput.value, placeInput.value)
+    initialCards.push({'name': placeInput.value,'link':linkInput.value});
 }
 
-function addCardformSubmit (evt, name, link) {
-    evt.preventDefault(); 
-    
+function closeOpenPopup(evt, btn = event.target){
+    if (btn.classList == 'profile__add-btn'){
+        popupCard.classList.add('popup_display_flex');
+    }
+    if (btn.classList == 'profile__settings'){
+        popup.classList.add('popup_display_flex');
+    }
+    if(btn.closest('.popup')){
+        btn.closest('.popup').classList.remove('popup_display_flex');
+    }
+}
+
+function addCard (link, name) {
     let galleryItem = galleryTemplate.cloneNode(true);
-    console.log(placeInput.value);
-    console.log(linkInput.value);
 
-    console.log(initialCards.length);
-    
-    initialCards.push({'name':placeInput.value,'link':linkInput.value});
-    console.log(initialCards[length+1]);
-    console.log(initialCards);
-
-    galleryItem.querySelector('.gallery__item-pic').src = linkInput.value;
-    galleryItem.querySelector('.gallery__item-title').textContent = placeInput.value;
+    galleryItem.querySelector('.gallery__item-pic').src = link;//linkInput.value;
+    galleryItem.querySelector('.gallery__item-title').textContent = name;//placeInput.value;
 
     galleryItem.querySelector('.gallery__item-like').addEventListener('click', function() {
         let target = event.target;
@@ -140,7 +93,6 @@ function addCardformSubmit (evt, name, link) {
     galleryItem.querySelector('.gallery__item-pic').addEventListener('click', function() {
         let target = event.target;
         if(target.classList == 'gallery__item-pic'){
-            console.log("Nryek yf rfhnbyre");
             let subtitle = target.closest('.gallery__item').querySelector('.gallery__item-title').textContent;
             let image = target.closest('.gallery__item').querySelector('.gallery__item-pic').src;
             popupFullImage.src = image;
@@ -174,11 +126,11 @@ function formSubmitHandler (evt) {
 }
 
 closeCross.forEach(item =>  {
-    console.log(item);
    item.addEventListener('click',closeOpenPopup);
  });
 
 popup.addEventListener('submit', formSubmitHandler); 
+
 addPlaceBtn.addEventListener('click', closeOpenPopup);
 settingsBtn.addEventListener('click', closeOpenPopup);
-popupCard.addEventListener('submit', addCardformSubmit); 
+popupCard.addEventListener('submit', addCardFormSubmit); 
